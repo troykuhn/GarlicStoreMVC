d21ebc3152fc27fed45e1b338600ddf13b4f70e8
Troy Kuhn <troykuhn22@gmail.com>
2019-02-04 15:08:36 -0500

    Add project files.

---
+++ GarlicStore.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GarlicStore.WebMVC.MvcApplication" Language="C#" %>
---
+++ GarlicStore.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarlicStore.WebMVC.Startup))]
+namespace GarlicStore.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
